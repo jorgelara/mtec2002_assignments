a3b05e0bca8a8416be9bb417a35184e154c08f3a
jorgelara <jdla68@gmail.com>
2012-02-01 19:09:55 -0500

    Update class1/labs/lpthw_ex1.py

---
+++ class1/labs/lpthw_ex1.py
@@ -0,0 +1,9 @@
+print "Hello world"
+print "hello again"
+print "I like typing this"
+print "Jorge Lara"
+print "this is fun"
+print "yay! printing"
+print "I'd rather you not"
+print 'I "said" do not touch this.'
+print 'jorge'
