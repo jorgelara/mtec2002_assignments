9df35986ef3aae2e0be0e97188d882b58eb484e3
jorgelara <jdla68@gmail.com>
2012-02-07 21:13:08 -0500

    Update class1/labs/lpthw_ex29.py

---
+++ class1/labs/lpthw_ex29.py
@@ -0,0 +1,27 @@
+people = 20
+cats = 30
+dogs = 15
+
+
+if people < cats:
+  print "Too many darn cats"
+  if people > cats:
+  print "God save the queen"
+  if people < dogs:
+    print "It's a dogs world after all"
+  if people > dogs:
+   print "Dog day afetrnoon at the park"
+  
+  dogs += 5
+  if people >= dogs:
+     print "People are greater than or equal to dogs."
+  if people <= dogs:
+     print "People are less than or equal to dogs."
+  if people == dogs:
+     print "People are dogs."
+     
+     Too many darn cats
+     God save the queen
+     People are greater than or equal to dogs.
+     People are less than or equal to dogs.
+     People are dogs.
